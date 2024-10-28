4a64a8123dde8768315c6d6d145c4ab56ccc8690
PedroCaravellos <142448150+PedroCaravellos@users.noreply.github.com>
2024-10-28 17:06:21 -0300

    Add files via upload

---
+++ Aula/codigo.js
@@ -0,0 +1,18 @@
+const container = document.getElementById('container');
+const montaCartao = (atleta) => `
+    <article>
+        <h1>${atleta.nome}<h1>
+        <img src=${atleta.imagem}>
+        <p>${atleta.detalhes}<p>
+    </article>
+`;
+let conteudo = ''
+
+/*for (let i=0;i < dados.length; i++) {
+    let atleta = dados[i];
+    conteudo += montaCartao(atleta);
+}*/
+
+dados.forEach(elemento => conteudo += montaCartao(elemento));
+
+container.innerHTML = conteudo;
